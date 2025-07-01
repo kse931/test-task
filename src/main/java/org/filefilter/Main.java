7ef09359924f3d051aef06b74eb659d66a040985
kse391 <kse04199@gmail.com>
2025-07-01 15:50:53 +0700

    inpit parameters and options

---
+++ src/main/java/org/filefilter/Main.java
@@ -0,0 +1,42 @@
+package org.filefilter;
+
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+
+@Command(name = "util", mixinStandardHelpOptions = true,
+        description = "Distributes different types of data from the input file across several")
+public class Main implements Callable<Integer> {
+
+    @Option(names = {"-o"}, description = "Output directory (current directory is used by default)")
+    private File outputDirectory = new File(".");
+
+    @Option(names = {"-p"}, description = "Prefix for output file")
+    private String prefix = "";
+
+    @Option(names = {"-a"}, description = "Add to existing files")
+    private boolean append = false;
+
+    @Option(names = {"-f"}, description = "Show full stats")
+    private boolean fullStats = false;
+
+    @Option(names = {"-s"}, description = "Show short stats")
+    private boolean shortStats = false;
+
+    @Parameters(arity = "1..*", description = "Input files")
+    private List<File> inputFiles;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
